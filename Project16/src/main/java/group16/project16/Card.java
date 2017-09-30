0a96a6ccb40ef7798d0bcf98f4ebe879e485965a
rfarr89 <ryanleefarrell@gmail.com>
2017-09-30 10:23:01 -0500

    Generated Card class. Cards are like nodes that hold text. They will eventually have more data associated with them.

---
+++ Project16/src/main/java/group16/project16/Card.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group16.project16;
+
+/**
+ * @author ryan
+ * 
+ * Cards are like nodes. They hold text at the moment.
+ *      Eventually they may be associated with an event, task, and/or attachment
+ */
+public class Card {
+    private String cardText;
+    private Card prevCard;
+    private Card nextCard;
+    
+    public Card(String text) {
+        this.cardText = text;
+        this.nextCard = null;
+        this.prevCard = null;
+    }
+    
+    public String getText() {
+        return this.cardText;
+    }
+    
+    public void changeText(String newText) {
+        this.cardText = newText;
+    }
+    
+    public void setNext(Card nCard) {
+        this.nextCard = nCard;
+    }
+    
+    public void setPrev(Card pCard) {
+        this.prevCard = pCard;
+    }
+     
+    public boolean hasNext() {
+        if (this.nextCard != null) {
+            return true;
+        }
+        else 
+            return false;
+    }
+    
+    public Card getNext() {
+        return this.nextCard;
+    }
+    
+    public boolean hasPrev() {
+        if (this.prevCard != null) {
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    public Card getPrev() {
+        return this.prevCard;
+    }
+}
