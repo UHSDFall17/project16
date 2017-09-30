04d6f0a0a958e8c273a54aeb8a278ac8ebc3fbd4
rfarr89 <ryanleefarrell@gmail.com>
2017-09-30 10:27:25 -0500

    Generated Board class. Boards are a double linked list that contains Lists.

---
+++ Project16/src/main/java/group16/project16/Board.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group16.project16;
+
+/**
+ * @author ryan
+ * 
+ */
+public class Board {
+    private String boardName;
+    private List headList;
+    private List tailList;
+    
+    public Board(String name) {
+        this.boardName = name;
+        List firstList = new List("To Do");
+        this.headList = firstList;
+        this.tailList = firstList;
+    }
+    
+    public String getName() {
+        return this.boardName;
+    }
+    
+    public void setName(String newName) {
+        this.boardName = newName;
+    }
+    
+    public void addList(List newList) {
+        // even though we added a first List when we made the Board, the user may
+        //      delete all the List, this if-else accounts for that
+        if (headList == null) {
+            headList = newList;
+            tailList = newList;
+        }
+        else {
+            tailList.setNext(newList);
+            newList.setPrev(tailList);
+            tailList = newList;
+        }
+    }
+    
+    public void removeList(List oldList) {
+        // check if headList
+        if (oldList != headList) {
+            // set the card before oldCard's next card to the one after oldCard
+            oldList.getPrev().setNext(oldList.getNext());
+        }
+        else {
+            // oldList == headList 
+            headList = oldList.getNext();
+            headList.setPrev(null);
+        }
+        // check if tailList
+        if (oldList != tailList) {
+            // set the card after oldCard's previous card to the one before oldCard
+            oldList.getNext().setPrev(oldList.getPrev());
+        }
+        else {
+            // oldList == tailList
+            tailList = oldList.getPrev();
+            tailList.setNext(null);
+        }
+        // now oldList is no longer in the linked list
+    }
+    
+}
