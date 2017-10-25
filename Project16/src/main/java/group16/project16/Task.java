4ffecda762b8549bfec2bd28f52dbc5b432a6816
rfarr89 <ryanleefarrell@gmail.com>
2017-10-25 17:08:40 -0500

    Generated an empty class for Task which extends class Card. Tasks are Cards but they have an associated due date.

---
+++ Project16/src/main/java/group16/project16/Task.java
@@ -0,0 +1,19 @@
+package group16.project16;
+
+/**
+ * Description:
+ *  Tasks are Cards with additional features such as a due date.
+ *  
+ * Author:
+ * 
+ */
+
+public class Task extends Card {
+    // Class attributes are private
+    
+    // Constructor is public. Include a call to the superclass (Card) in the constructor.
+    
+    // Include methods for getting and setting due date. 
+       
+    // Include any other methods you think will be of help. Remember we are not doing anything System.out.print() unless the professor changes his mind.
+}
