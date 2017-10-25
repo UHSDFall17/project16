542c3a266141cfa8d45fda83df7096aec26bfc16
rfarr89 <ryanleefarrell@gmail.com>
2017-10-25 17:31:52 -0500

    Changed public access modifiers to default (undeclared = package private in java) so they may only be used within our package. Also removed printBoard() as we were instructed to not include print statements at the last demo.

--- Project16/src/main/java/group16/project16/Board.java
+++ Project16/src/main/java/group16/project16/Board.java
@@ -14,29 +14,29 @@ public class Board {
     private List headList;
     private List tailList;
     
-    public Board(String name) {
+    Board(String name) {
         this.boardName = name;
         //List firstList = new List();
         this.headList = null;
         this.tailList = null;
     }
     
-    public Board(String bName, String lName) {
+    Board(String bName, String lName) {
         this.boardName = bName;
         List firstList = new List(lName);
         this.headList = firstList;
         this.tailList = firstList;
     }
     
-    public String getName() {
+    String getName() {
         return this.boardName;
     }
     
-    public void setName(String newName) {
+    void setName(String newName) {
         this.boardName = newName;
     }
     
-    public void addList(List newList) {
+    void addList(List newList) {
         // even though we added a first List when we made the Board, the user may
         //      delete all the List, this if-else accounts for that
         //List newList = new List(nList.getName());
@@ -51,7 +51,7 @@ public class Board {
         }
     }
     
-    public void removeList(List oldList) {
+    void removeList(List oldList) {
         // check if headList
         if (oldList != headList) {
             // set the card before oldCard's next card to the one after oldCard
@@ -75,29 +75,4 @@ public class Board {
         // now oldList is no longer in the linked list
     }
     
-    public void printBoard() {
-        if (this.boardName == null) {
-            System.out.println("There was a problem retrieving your Board...");
-            return; // trying to catch bad calls to printList()
-        }
-        // when printList() is called it prints the List name and all of its Cards to console
-        System.out.println("********************************");
-        System.out.println("********************************");
-        System.out.println("Board: " + this.boardName);
-        System.out.println("********************************");
-        List printList = null;        // printList is a List used to walk through the Board
-        printList = this.headList;          // printList starts at headList, might be null
-        int index = 0;
-        while (printList != null) {
-            System.out.println(index + ": " + printList);
-            if (printList.hasNext()) {
-                printList = printList.getNext();
-                index++;
-            }
-            else {
-                break;  // there is not a next List to print, move forward
-            }
-        }
-        System.out.println("********* End of Board *********");
-    }
 }
