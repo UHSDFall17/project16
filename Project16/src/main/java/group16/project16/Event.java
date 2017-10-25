e2b4ba2d52fa8425426618f83328e2ad0b982903
rfarr89 <ryanleefarrell@gmail.com>
2017-10-25 17:07:55 -0500

    Generated an empty class for Event which extends class Card. Events are Cards but they have associated start and stop times as well as a location.

---
+++ Project16/src/main/java/group16/project16/Event.java
@@ -0,0 +1,19 @@
+package group16.project16;
+
+/**
+ * Description:
+ *  Events are Cards with additional features such as a start time, stop time, and location.
+ *  
+ * Author:
+ * 
+ */
+
+public class Event extends Card {
+    // Class attributes are private
+    
+    // Constructor is public. Include a call to the superclass (Card) in the constructor.
+    
+    // Include methods for getting and setting start and stop times as well as location. 
+       
+    // Include any other methods you think will be of help. Remember we are not doing anything System.out.print() unless the professor changes his mind.
+}
