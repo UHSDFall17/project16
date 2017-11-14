ad74e96141d624eca83d6ff7a272ecdf48203dbb
rfarr89 <ryanleefarrell@gmail.com>
2017-11-14 12:12:33 -0600

    Modified CardTest to reflect modifications to class Card since the addition of class Node.

--- Project16/src/test/java/group16/project16/CardTest.java
+++ Project16/src/test/java/group16/project16/CardTest.java
@@ -1,5 +1,8 @@
-/*
+/**
+ * @author ryan
  * Test Class for Class Card. Very basic functionality tests.
+ * More realistic test case(s) will be generated to test multiple classes when 
+ * each class has been proven by a basic test. 
  */
 package group16.project16;
 
@@ -10,10 +13,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author ryan
- */
 public class CardTest {
     
     public CardTest() {
@@ -40,11 +39,10 @@ public class CardTest {
      */
     @Test
     public void testGetText() {
-        System.out.println("getText");
-        Card instance = new Card();
-        String expResult = "";
+        String cardText = "card text";
+        Card instance = new Card(cardText);
         String result = instance.getText();
-        assertEquals(expResult, result);
+        assertEquals(cardText, result);
     }
 
     /**
@@ -52,86 +50,11 @@ public class CardTest {
      */
     @Test
     public void testChangeText() {
-        System.out.println("changeText");
-        String newText = "";
-        Card instance = new Card();
+        String originalText = "original";
+        String newText = "new";
+        Card instance = new Card(originalText);
         instance.changeText(newText);
-        String result = instance.getText();
-        assertEquals(newText, result);
-    }
-
-    /**
-     * Test of setNext method, of class Card.
-     */
-    @Test
-    public void testSetNext() {
-        System.out.println("setNext");
-        Card nCard = new Card();
-        Card instance = new Card();
-        instance.setNext(nCard);
-        Card result = (Card)instance.getNext();
-        assertEquals(nCard, result);
-    }
-
-    /**
-     * Test of setPrev method, of class Card.
-     */
-    @Test
-    public void testSetPrev() {
-        System.out.println("setPrev");
-        Card pCard = new Card();
-        Card instance = new Card();
-        instance.setPrev(pCard);
-        Card result = (Card)instance.getPrev();
-        assertEquals(pCard, result);
-    }
-
-    /**
-     * Test of hasNext method, of class Card.
-     */
-    @Test
-    public void testHasNext() {
-        System.out.println("hasNext");
-        Card instance = new Card();
-        boolean expResult = false;
-        boolean result = instance.hasNext();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getNext method, of class Card.
-     */
-    @Test
-    public void testGetNext() {
-        System.out.println("getNext");
-        Card instance = new Card();
-        Card expResult = null;
-        Card result = (Card)instance.getNext();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of hasPrev method, of class Card.
-     */
-    @Test
-    public void testHasPrev() {
-        System.out.println("hasPrev");
-        Card instance = new Card();
-        boolean expResult = false;
-        boolean result = instance.hasPrev();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getPrev method, of class Card.
-     */
-    @Test
-    public void testGetPrev() {
-        System.out.println("getPrev");
-        Card instance = new Card();
-        Card expResult = null;
-        Card result = (Card)instance.getPrev();
-        assertEquals(expResult, result);
+        assertNotEquals(originalText, instance.getText());
     }
     
 }
