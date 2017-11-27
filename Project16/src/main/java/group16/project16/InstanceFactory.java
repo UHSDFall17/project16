07a7452062df6ba244ef45c2f9d2c5add28a7448
rfarr89 <ryanleefarrell@gmail.com>
2017-11-27 10:25:08 -0600

    WORK IN PROGRESS. Adding InstanceFactory to build a User instance (login, loading, and saving). Uses class User. Trying to implement Factory and Singleton design patterns.

---
+++ Project16/src/main/java/group16/project16/InstanceFactory.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group16.project16;
+
+/**
+ *  InstanceFactory is a Factory for User instances (a login session). Sets up
+ * Board, List, and Card objects for the User that is logged in.
+ * 
+ * @author ryan
+ */
+public class InstanceFactory {
+/*    
+    public User getUser() {
+        String uName = User.loginSequence();        // verifies User credentials and returns the User's name is legitimate
+        if (uName.equals("Invalid_Login")) {       
+            return null;                            // return null if the User did not log in correctly
+        }
+        else {
+            // login is valid, finish building instance
+            User thisUser = new User(uName, uPass); // we don't have pass... do we need it beyond login?
+            thisUser.loadData();    // attempt to load data for that user
+        }
+        
+    }
+*/
+    
+    
+}
