0794acc3c04ef64f1139e8058f5d285436728ae9
rfarr89 <ryanleefarrell@gmail.com>
2017-11-27 20:04:42 -0600

    Adding UserTest for class User. Not all tests are working. The failing tests are currently commented out.

---
+++ Project16/src/test/java/group16/project16/UserTest.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group16.project16;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.io.ByteArrayInputStream;
+
+/**
+ *
+ * @author ryan
+ */
+public class UserTest {
+    
+    public UserTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getUser method, of class User.
+     */
+    @Test
+    public void testGetUser() {
+        User expResult = null;
+        User result = User.getUser();
+        assertNotEquals(expResult, result);
+    }
+    
+    /**
+     * Test of getUserName method, of class User.
+     */
+    @Test
+    public void testGetUserName() {
+        User expResult = null;
+        String result = User.getUserName();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of UserFactory method, of class User.
+    
+    @Test
+    public void testUserFactory() {
+        String testInputs1 = "Y" + System.lineSeparator() + "testName1" + System.lineSeparator() + "password1!";
+        ByteArrayInputStream in = new ByteArrayInputStream(testInputs1.getBytes());
+        System.setIn(in);
+        User testUser = User.UserFactory();
+        String expResult = "testName1";
+        String resultName = User.getUserName();
+        assertEquals(expResult, resultName);
+        System.setIn(System.in);
+    } */
+
+    /**
+     * Test of LogOutFactory method, of class User.
+     
+    @Test
+    public void testLogOutFactory() {
+        User testUser = User.UserFactory();
+        User.LogOutFactory();
+        assertEquals(null, testUser.getRootBoard());
+    }*/
+
+    /**
+     * Test of getRootBoard method, of class User.
+     */
+    @Test
+    public void testGetRootBoard() {
+        User instance = User.getUser();
+        Board expResult = null;
+        Board result = instance.getRootBoard();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of addBoard method, of class User.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testAddBoard() {
+        Board newBoard = null;
+        User instance = null;
+        instance.addBoard(newBoard);
+    }
+
+    /**
+     * Test of removeBoard method, of class User.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testRemoveBoard() {
+        Board oldBoard = null;
+        User instance = null;
+        instance.removeBoard(oldBoard);
+    }
+
+}
